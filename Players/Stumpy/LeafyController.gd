3ba455e9c990afa23e80dc7b6179071bc0b64c82
Chris Crain <chrisac309@gmail.com>
2020-08-04 00:09:23 -0400

    Leafy custom targeting
    Targets First, Last, Near, and Far

--- Players/Stumpy/LeafyController.gd
+++ Players/Stumpy/LeafyController.gd
@@ -10,6 +10,34 @@ onready var ySort = parent.get_parent()
 
 var spawnedLeafy = []
 var targets = []
+var previousClosest
+var previousFarthest
+
+enum {
+	FIRST,
+	LAST,
+	NEAR,
+	FAR
+}
+
+export var TARGETING_TYPE = FIRST
+
+func _process(delta):
+	update_targets()
+
+# Nearest and farthest need updated on a per-frame basis
+func update_targets():
+	var newTarget
+	if TARGETING_TYPE == NEAR:
+		newTarget = get_closest_target()
+		if newTarget != null && newTarget != previousClosest:
+			reassign_leafys_targeting_body(previousClosest)
+			previousClosest = newTarget
+	elif TARGETING_TYPE == FAR:
+		newTarget = get_farthest_target()
+		if newTarget != null && newTarget != previousFarthest:
+			reassign_leafys_targeting_body(previousFarthest)
+			previousFarthest = newTarget
 
 func spawn_leafy():
 	if spawnedLeafy.size() < MAX_LEAFY:
@@ -36,16 +64,53 @@ func reassign_leafys_targeting_body(body):
 		if leafy.target_movement.get_target() == body:
 			assign_leafy_target(leafy)
 
+# Assigns the passed leafy to a new target depending on targeting type
 func assign_leafy_target(leafy):
-	var earliestTarget = targets.front()
-	if earliestTarget != null:
-		set_single_leafy_target(leafy, earliestTarget)
+	var newTarget
+	match TARGETING_TYPE:
+		FIRST:
+			newTarget = targets.front()
+		LAST:
+			newTarget = targets.back()
+		NEAR:
+			newTarget = get_closest_target()
+		FAR:
+			newTarget = get_farthest_target()
+
+	if newTarget != null:
+		set_single_leafy_target(leafy, newTarget)
 	else:
 		set_single_leafy_target(leafy, parent)
+
+func get_farthest_target():
+	var farthest_enemy = null
+	var max_distance = -1
+
+	for enemy in targets:
+		var distance = parent.global_position.distance_squared_to(enemy.global_position)
+		if distance > max_distance:
+			max_distance = distance
+			farthest_enemy = enemy
+
+	return farthest_enemy
+	
+func get_closest_target():
+	var closest_enemy = null
+	var min_distance = INF
+
+	for enemy in targets:
+		var distance = parent.global_position.distance_squared_to(enemy.global_position)
+		if distance < min_distance:
+			min_distance = distance
+			closest_enemy = enemy
+
+	return closest_enemy
 
 func _on_EnemyDetector_body_entered(body):
 	targets.append(body)
 	reassign_leafys_targeting_body(parent)
+	if TARGETING_TYPE == LAST:
+		set_leafy_targets(body)
 
 func _on_EnemyDetector_body_exited(body):
 	targets.erase(body)
