fb4484be82e0588b2da51d61ae255de379b0d605
Chris Crain <chrisac309@gmail.com>
2020-08-07 17:20:23 -0400

    Enemy follows player

--- Enemies/Troll.gd
+++ Enemies/Troll.gd
@@ -1,17 +1,12 @@
-extends KinematicBody2D
-
-onready var target_movement = $TargetMovement
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+extends "./Enemy.gd"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	target_movement.follow(delta)
+
+func _on_RangeDetector_body_entered(body):
+	target_movement.set_target(body)
