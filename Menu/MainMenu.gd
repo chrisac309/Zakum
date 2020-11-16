372039fca49c72051680c45361070c24e83f6ec6
Chris Crain <chrisac309@gmail.com>
2020-11-15 20:35:49 -0500

    Preparing for mobile version

--- Menu/MainMenu.gd
+++ Menu/MainMenu.gd
@@ -2,13 +2,7 @@ extends MarginContainer
 
 const character_select_scene = preload("res://Menu/CharacterSelect/CharacterSelect.tscn")
 
-func _on_Singleplayer_button_up():
-	PlayerData.player_count = 1
-	SceneChanger.change_scene(character_select_scene)
-
-func _on_Multiplayer_button_up():
-	PlayerData.player_count = 2
-	SceneChanger.change_scene(character_select_scene)
-
-func _on_Exit_Game_button_up():
-	get_tree().quit()
+func _on_MainMenu_gui_input(event):
+	if event is InputEventScreenTouch:
+		SceneChanger.change_scene(character_select_scene)
+		print("Going to the next scene")
